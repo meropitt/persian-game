e54f0a03ccb1ebbd284ba1a2498abaa125e084fd
Mero Pitt <92892760+meropitt@users.noreply.github.com>
2026-09-15 12:10:47 +0300

    Update Supabase URL in config.js

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
 // ضع بيانات مشروع Supabase الخاص بك هنا
 // تجدها في: Supabase Dashboard → Project Settings → API
-export const SUPABASE_URL = "https://hzamthtgiwmlanyyiwdh.supabase.co/rest/v1/";
+export const SUPABASE_URL = "https://hzamthtgiwmlanyyiwdh.supabase.co";
 export const SUPABASE_ANON_KEY = "sb_publishable_GsI6ZjO7kxYh9cWsK8nfJQ_wyYgWa9c";
